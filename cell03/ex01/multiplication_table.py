19ff96d36da79adb089ea5893ad087553851716b
Anjo Genow <agenow@student.42berlin.de>
2024-06-10 16:12:21 +0200

    typechecking input

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
-num = int(input("Enter a number\n"))
-for i in range (0, 10):
-    print(i, "x", num, "=", i*num)
+try:
+    num = int(input("Enter a number\n"))
+    for i in range (0, 10):
+        print(i, "x", num, "=", i*num)
+except ValueError:
+    print("Number!")
